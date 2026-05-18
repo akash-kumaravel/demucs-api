b70f28972210f3b73a90bb72d42788a4a405547e
Akash <akashuxui@gmail.com>
2026-05-18 10:52:09 +0530

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,10 +1,31 @@
 import runpod
+import subprocess
+import uuid
+import os
 
 def handler(job):
+
     job_input = job["input"]
 
+    audio_url = job_input.get("audio_url")
+
+    if not audio_url:
+        return {"error": "audio_url missing"}
+
+    filename = f"{uuid.uuid4()}.mp3"
+
+    subprocess.run(
+        ["wget", audio_url, "-O", filename],
+        check=True
+    )
+
+    subprocess.run(
+        ["demucs", filename],
+        check=True
+    )
+
     return {
-        "message": "working"
+        "message": "separation completed"
     }
 
 runpod.serverless.start({"handler": handler})
